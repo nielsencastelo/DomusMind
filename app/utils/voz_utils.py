abc96ce075be6c09344a59be1ae8b0394888fc75
Nielsen Castelo Damasceno Dantas <nielsen.castelo@inatel.br>
2025-04-18 11:20:01 -0300

    testando tts e LLL juntos

--- app/utils/voz_utils.py
+++ app/utils/voz_utils.py
@@ -1,17 +1,30 @@
-# fala com TTS Coqui
-from TTS.api import TTS
-import os
-import tempfile
-import sounddevice as sd
-import soundfile as sf
+from transformers import VitsModel, AutoTokenizer
+import torch
+import io
+from IPython.display import Audio
+import scipy.io.wavfile
+import numpy as np
+from pydub import AudioSegment
+from IPython.display import Audio
 
-tts = TTS(model_name="tts_models/pt/cv/vits", progress_bar=False, gpu=False)
+# Carrega modelo e tokenizer
+model = VitsModel.from_pretrained("facebook/mms-tts-por")
+tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-por")
+
 
 def falar_texto(texto):
-    with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as tmpfile:
-        path = tmpfile.name
-        tts.tts_to_file(text=texto, file_path=path)
-        data, fs = sf.read(path)
-        sd.play(data, fs)
-        sd.wait()
-        os.remove(path)
+    
+    inputs = tokenizer(texto, return_tensors="pt")
+    with torch.no_grad():
+        output = model(**inputs).waveform
+
+    # Normaliza e converte para int16
+    audio = output.squeeze().numpy()
+    audio = audio / np.max(np.abs(audio))  # normaliza para [-1, 1]
+    audio_int16 = (audio * 32767).astype(np.int16)
+
+    # buffer = io.BytesIO()
+    # scipy.io.wavfile.write(buffer, rate=model.config.sampling_rate, data=audio_int16)
+    
+    return audio_int16, model
+
