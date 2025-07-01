37b2636897f34956b232daf389a4ad11d8e4d6c1
Nielsen Castelo Damasceno Dantas <nielsen.castelo@inatel.br>
2025-07-01 16:15:48 -0300

    removendo arquivos não necessários

--- app/mqtt_controller.py
+++
@@ -1,9 +0,0 @@
-# publicação no MQTT
-import paho.mqtt.client as mqtt
-
-client = mqtt.Client()
-client.connect("localhost", 1883)
-
-def enviar_acao(topico, comando):
-    print(f"[MQTT] Enviando para {topico}: {comando}")
-    client.publish(topico, comando)
