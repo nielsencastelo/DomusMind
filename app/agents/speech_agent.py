4ae46d6476b9957fc2ae24d4c9c9331d8052c823
Nielsen Castelo Damasceno Dantas <nielsen.castelo@inatel.br>
2025-10-28 20:06:50 -0300

    updates tts audio

--- app/agents/speech_agent.py
+++ app/agents/speech_agent.py
@@ -1,6 +1,6 @@
 # app/agents/speech_agent.py
-from utils.voz_utils import speak_text_with_mms
+from utils.voz_utils import speak_text_with_tts
 
 class SpeechAgent:
     def speak(self, text: str):
-        speak_text_with_mms(text)
+        speak_text_with_tts(text)
--- app/utils/voz_utils.py
+++ app/utils/voz_utils.py
@@ -1,20 +1,34 @@
-from transformers import VitsModel, AutoTokenizer
+# from transformers import VitsModel, AutoTokenizer
 import torch
 import numpy as np
 import sounddevice as sd
+from TTS.api import TTS
 
-model = VitsModel.from_pretrained("facebook/mms-tts-por")
-tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-por")
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-def speak_text_with_mms(text):
-    
-    inputs = tokenizer(text, return_tensors="pt")
-    with torch.no_grad():
-        output = model(**inputs).waveform
+tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2", progress_bar=False).to(device)
 
-    audio = output.squeeze().numpy()
-    audio = audio / np.max(np.abs(audio))  # normaliza para [-1, 1]
+def speak_text_with_tts(text):
+    wav = tts.tts(text = text, speaker_wav="Voz_Nielsen.wav", language="pt", )
+    sr = getattr(getattr(tts, "synthesizer", None), "output_sample_rate", None) or 24000
 
-    print("🔊 Reproduzindo resposta...")
-    sd.play(audio, samplerate=model.config.sampling_rate)
+    wav = np.asarray(wav).astype(np.float32).reshape(-1)
+    sd.play(wav, sr)
     sd.wait()
+
+
+# model = VitsModel.from_pretrained("facebook/mms-tts-por")
+# tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-por")
+
+# def speak_text_with_mms(text):
+    
+#     inputs = tokenizer(text, return_tensors="pt")
+#     with torch.no_grad():
+#         output = model(**inputs).waveform
+
+#     audio = output.squeeze().numpy()
+#     audio = audio / np.max(np.abs(audio))  # normaliza para [-1, 1]
+
+#     print("🔊 Reproduzindo resposta...")
+#     sd.play(audio, samplerate=model.config.sampling_rate)
+#     sd.wait()
