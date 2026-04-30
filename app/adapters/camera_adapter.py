8bf1d98a26393b4224dd507be8ac403cb927d051
Nielsen <nielsen.tekla@gmail.com>
2026-04-30 18:41:41 -0300

    organizer cods

--- app/adapters/camera_adapter.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-
-class CameraAdapter:
-    def open(self, source: str | int):
-        if isinstance(source, str) and source.isdigit():
-            source = int(source)
-        return cv2.VideoCapture(source)
